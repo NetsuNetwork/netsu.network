161e246c4fb9b6e6897e2f18ed678da8aa3c4b67
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-05 11:58:13 +1000

    chore: CDN

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-  ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      maxWidth: {
+        fit: '85vw',
+      },
       typography: ({ theme }) => ({
         zinc: {
           '--tw-prose-pre-bg': 'transparent',
