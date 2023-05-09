72da5cb9d90866aaa27cc210af35f1ff8a250304
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-09 21:12:34 +1000

    feat: Button to blog / posts

--- src/components/MetaButtons.svelte
+++ src/components/MetaButtons.svelte
@@ -1,10 +1,13 @@
 <div class="grid grid-flow-col mt-5">
-  <a href="https://dev.astolfo.cafe/?utm_source=netsu.network&utm_medium=referral&utm_campaign=presence" class="ml-4 text-lg text-white button"
-    >Affiliate</a
+  <a
+    href="https://dev.astolfo.cafe/?utm_source=netsu.network&utm_medium=referral&utm_campaign=presence"
+    class="ml-4 text-lg text-white button">Affiliate</a
   >
-  <a href="https://github.com/NetsuNetwork" class="ml-4 text-lg text-white button"
-    >GitHub</a
+  <a
+    href="https://github.com/NetsuNetwork"
+    class="ml-4 text-lg text-white button">GitHub</a
   >
+  <a href="/posts" class="ml-4 text-lg text-white button"> Posts </a>
 </div>
 
 <style lang="scss">
