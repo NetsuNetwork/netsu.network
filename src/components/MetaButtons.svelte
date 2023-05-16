a9d15f70bbcb7f8a378a958c8fcf2a3be13b0445
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-17 09:34:51 +1000

    refactor: owo styling

--- src/components/MetaButtons.svelte
+++ src/components/MetaButtons.svelte
@@ -1,13 +1,13 @@
-<div class="grid grid-flow-col mt-5">
+<div class="grid grid-flow-col mt-10">
   <a
     href="https://dev.astolfo.cafe/?utm_source=netsu.network&utm_medium=referral&utm_campaign=presence"
-    class="ml-4 text-lg text-white button">Affiliate</a
+    class="ml-4 text-lg text-white button rounded-s-full">Affiliate</a
   >
   <a
     href="https://github.com/NetsuNetwork"
     class="ml-4 text-lg text-white button">GitHub</a
   >
-  <a href="/posts" class="ml-4 text-lg text-white button" rel='prefetch'> Posts </a>
+  <a href="/posts" class="ml-4 text-lg text-white button rounded-e-full" rel='prefetch'> Posts </a>
 </div>
 
 <style lang="scss">
@@ -15,7 +15,7 @@
     .button {
       text-decoration: none;
       border-width: 3px;
-      padding: 5px;
+      padding: 10px;
     }
   }
 </style>
