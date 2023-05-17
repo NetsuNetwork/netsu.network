6d6575720bb53c6e3e51c3b9e37725d67b9b67de
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-17 10:45:13 +1000

    refactor: Use gap instead of margins

--- src/components/MetaButtons.svelte
+++ src/components/MetaButtons.svelte
@@ -1,13 +1,13 @@
-<div class="grid grid-flow-col mt-10">
+<div class="grid grid-flow-col mt-10 gap-3">
   <a
     href="https://dev.astolfo.cafe/?utm_source=netsu.network&utm_medium=referral&utm_campaign=presence"
-    class="ml-4 text-lg text-white button rounded-s-full">Affiliate</a
+    class="text-lg text-white button rounded-s-full">Affiliate</a
   >
   <a
     href="https://github.com/NetsuNetwork"
-    class="ml-4 text-lg text-white button">GitHub</a
+    class="text-lg text-white button">GitHub</a
   >
-  <a href="/posts" class="ml-4 text-lg text-white button rounded-e-full" rel='prefetch'> Posts </a>
+  <a href="/posts" class="text-lg text-white button rounded-e-full" rel='prefetch'> Posts </a>
 </div>
 
 <style lang="scss">
